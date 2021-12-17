0b833d46e818a99c60fc00c3a3b5eea98f6ec7fc
unknown <neetamourya24@gmail.com>
2021-12-17 23:43:31 +0530

    queue and dequeue

--- src/main/java/com/bridgelabz/datastructure/stackandqueue/StackAndQueue.java
+++ src/main/java/com/bridgelabz/datastructure/stackandqueue/StackAndQueue.java
@@ -1,6 +1,9 @@
 package com.bridgelabz.datastructure.stackandqueue;
 import java.util.EmptyStackException;
+import java.util.Iterator;
+import java.util.PriorityQueue;
 import java.util.Stack;  
+import java.util.*;  
 public class StackAndQueue { 
 	public static void main(String args[])   
 	{  
@@ -25,6 +28,8 @@ public class StackAndQueue {
 	}  
 	StackAndQueue queue=new StackAndQueue();
 	queue.peek();
+	queue.queue();
+	queue.deque();
 	}  
 	//performing push operation  
 	static void add(Stack stk, int x)   
@@ -57,4 +62,34 @@ public class StackAndQueue {
 	//prints stack  
 	System.out.println("Element at top: " + fruits);  
 	}
+	public void queue() {
+	PriorityQueue<String> queue=new PriorityQueue<String>();  
+	queue.add("56");  
+	queue.add("30");  
+	queue.add("70");    
+	System.out.println("head:"+queue.element());  
+	System.out.println("head:"+queue.peek());  
+	System.out.println("iterating the queue elements:");  
+	Iterator itr=queue.iterator();  
+	while(itr.hasNext()){  
+	System.out.println(itr.next());  
 	}  
+	queue.remove();  
+	queue.poll();  
+	System.out.println("after removing two elements:");  
+	Iterator<String> itr2=queue.iterator();  
+	while(itr2.hasNext()){  
+	System.out.println(itr2.next());  
+	}  
+	}
+	public void deque() {
+	Deque<String> deque = new ArrayDeque<String>();  
+	   deque.add("56");    
+	   deque.add("30");     
+	   deque.add("70");    
+	   //Traversing elements  
+	   for (String str : deque) {  
+	   System.out.println("Deque list:"+str);  
+	   }  
+	   }  
+}
