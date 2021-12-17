c361fca2376e605ec62413e840f7ee1f56c5cf62
unknown <neetamourya24@gmail.com>
2021-12-17 19:15:46 +0530

    [Neeta mourya] insert element between list UC4 branch

--- src/main/java/com/bridgelabz/datastructure/linkedlist/LinkedList.java
+++ src/main/java/com/bridgelabz/datastructure/linkedlist/LinkedList.java
@@ -2,21 +2,17 @@ package com.bridgelabz.datastructure.linkedlist;
 //Java program to implement
 //a Singly Linked List
 import java.io.*;
-
 //Class 1
 //Helper Class (Generic node class for LinkedList)
 class Node<T> {
-
 // Data members
 // 1. Storing value of node
 T data;
 // 2. Storing address of next node
 Node<T> next;
-
 // Parameterized constructor to assign value
 Node(T data)
 {
-
    // This keyword refers to current object itself
    this.data = data;
    this.next = null;
@@ -25,12 +21,10 @@ Node(T data)
 //Class 2
 //Helper class ( Generic LinkedList class)
 class list<T> {
-
 // Generic node instance
 Node<T> head;
 // Data member to store length of list
 private int length = 0;
-
 // Default constructor
 list() 
 { this.head = null; }
@@ -116,6 +110,8 @@ void add(int position, T data)
   // new value now points to former current node
   prev.next.next = temp;
 }
+// Method
+// To add new node at any given position
 }
 //Main Class
 public class LinkedList {
@@ -125,25 +121,39 @@ public static void main(String[] args)
 {
 	System.out.println("Welcome message in datastructure linked list prog");
 	 // Integer List
-
     // Creating new empty Integer linked list
     list<Integer> list1 = new list<>();
-    System.out.println(
-        "Integer LinkedList created as list1 :");
+    System.out.println("Integer LinkedList created as list1 :");
     // Adding elements to the above List object
-
     // Element 1 - 56
     list1.add(56);
     // Element 2 - 30
     list1.add(30);
     // Element 3 - 70
     list1.add(70);
-
     // Display message only
-    System.out.println(
-        "list1 after adding 56,30 and 70 :");
-
+    System.out.println("list1 after adding 56,30 and 70 :");
     // Print and display the above List elements
     System.out.println(list1);
+ // String LinkedList
+    // Creating new empty String linked list
+    list<String> list2 = new list<>();
+    System.out.println("String LinkedList created as list2");
+    // Adding elements to the above List object
+    // Element 1 - 56
+    list2.add("56");
+    // Element 3 - 70
+    list2.add("70");
+    // Display message only
+    System.out.println("list2 after adding 56 and 70 :");
+    // Print current elements only
+    System.out.println(list2);
+    // Now, adding element 2- "30" at position 2
+    list2.add(2, "30");
+    // Display message only
+    System.out.println("list2 after adding 30 at position 2 :");
+    // now print the updated List again
+    // after inserting element at second position
+    System.out.println(list2);
 	}
 }
