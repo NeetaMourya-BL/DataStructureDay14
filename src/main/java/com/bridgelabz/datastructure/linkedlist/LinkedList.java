38a9599edf6a5501b68cf52b9072afbaf4128dce
unknown <neetamourya24@gmail.com>
2021-12-17 18:46:28 +0530

    [Neeta mourya] create generic

--- src/main/java/com/bridgelabz/datastructure/linkedlist/LinkedList.java
+++ src/main/java/com/bridgelabz/datastructure/linkedlist/LinkedList.java
@@ -5,16 +5,16 @@ import java.io.*;
 
 //Class 1
 //Helper Class (Generic node class for LinkedList)
-class node<T> {
+class Node<T> {
 
 // Data members
 // 1. Storing value of node
 T data;
 // 2. Storing address of next node
-node<T> next;
+Node<T> next;
 
 // Parameterized constructor to assign value
-node(T data)
+Node(T data)
 {
 
    // This keyword refers to current object itself
