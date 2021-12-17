78a50bd55e6fa64c93b852dd1dc184bcac1c2aa9
unknown <neetamourya24@gmail.com>
2021-12-17 18:41:33 +0530

    [Neeta mourya] welcome message in master branch

---
+++ src/main/java/com/bridgelabz/datastructure/linkedlist/LinkedList.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.datastructure.linkedlist;
+//Main Class
+public class LinkedList {
+
+// main method
+public static void main(String[] args)
+{
+	System.out.println("Welcome message in datastructure linked list prog");
+	}
+}
