0f9619faaad23018836798749a724e33aedb39ae
unknown <neetamourya24@gmail.com>
2021-12-17 23:03:26 +0530

    [Neeta mourya] welcome message in main

---
+++ src/main/java/com/bridgelabz/datastrucutre/stackqueue/StackAndQueue.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.datastrucutre.stackqueue;
+
+public class StackAndQueue {
+	public static void main(String[] args) {
+		System.out.println("Welcome to data structure stack and queue program");
+	}
+
+}
